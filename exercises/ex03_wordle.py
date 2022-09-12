b053455d8c192ba7f11539c4e84aad22b6e1a40e
Allison Ranatza <aranatza@ad.unc.edu>
2022-09-11 22:19:34 -0400

    Finished Ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,28 +1,27 @@
-"""Ex03 - Wordle"""
+"""Ex03 - Wordle."""
 
 __author__ = "730560370"
 
+
 def contains_char(word: str, letter: str) -> bool: 
-    """a function for searching a string for any given letter"""
+    """A function for searching a string for any given letter."""
     assert len(letter) == 1
     i: int = 0
     while i < len(word):
         if letter == word[i]:
             return True
         i += 1
-        if i == len(word):
-            return False                        # checks that any given letter is or isn't somewhere in the program's word
+    return False                        # checks that any given letter is or isn't somewhere in the program's word
+
 
 def emojified(guess: str, word: str) -> str:
-    """a function to represent correctly placed, wrongly placed, and incorrect letters, as emojis"""
-    assert len(word) == len(guess)
-    
+    """A function to represent correctly placed, wrongly placed, and incorrect letters, as emojis."""
+    assert len(guess) == len(word) 
     green_box: str = "\U0001F7E9"
     yellow_box: str = "\U0001F7E8"   
     white_box: str = "\U00002B1C"               # creating emojis
     i: int = 0
     result: str = ""                            # outputs emojis on the same line
-   
     while i < len(word):
         if contains_char(word, guess[i]) is True:
             if word[i] == guess[i]: 
@@ -32,32 +31,35 @@ def emojified(guess: str, word: str) -> str:
         if contains_char(word, guess[i]) is False:
             result += white_box                 # incorrect letter
         i += 1
-    return print(result)
+    return result
+
 
 def input_guess(expected_length: int) -> str:
-    """prompts for a user's guess, assures the guess is of the correct length"""
+    """Prompts for a user's guess, assures the guess is of the correct length."""
     guess = str(input(f"Enter a {expected_length} character word: "))
     while len(guess) != expected_length:
-        guess = (input(f"That wasn't {expected_length} chars! Try again: ")) #ensures the program's word and the user's guess are of equal length
+        guess = (input(f"That wasn't {expected_length} chars! Try again: "))    # ensures the program's word and the user's guess are of equal length
     return guess
+
 
 def main() -> None:
     """The entrypoint of the program and main game loop."""
     word: str = "codes"
     turn_number: int = 1
     correct_guess: bool = False
-
     while turn_number <= 6 and correct_guess is False:
         print(f"=== Turn {turn_number}/6 ===") 
         guess: str = (input_guess(len(word)))   # guess is of the correct length via input_guess function
         emojified(guess, word)                  # guess correctness is shown in emojis via emojified
-        if guess == word:                       # winning guess, end of program
-           correct_guess = True
-           print(f"You won in {turn_number}/6 turns!")
+        if guess == word:                      
+            correct_guess = True
+            print(f"You won in {turn_number}/6 turns!")
+            return None                          # winning guess, end of program
         turn_number += 1
-
-    if turn_number > 6 and correct_guess is False:  # false guess, end of program
+    if turn_number > 6 and correct_guess is False:  
         print("X/6 - Sorry, try again tomorrow!")
+        return None                             # false guess, end of program
+
 
 if __name__ == "__main__":                      # calls the main function and actually runs the program
     main()
