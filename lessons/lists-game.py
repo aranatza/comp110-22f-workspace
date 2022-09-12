940fe105e7b370b6e9e9f2453f0dbb6c05d976a9
Allison Ranatza <aranatza@ad.unc.edu>
2022-09-12 00:06:39 -0400

    list-game finished

--- lessons/lists-game.py
+++ lessons/lists-game.py
@@ -18,8 +18,8 @@ print(rolls)
 i: int = 0
 sum: int = 0
 while i < len(rolls):
-    sum += rolls[i]
-    i += 1
+    sum = sum + rolls[i]
+    i = i + 1
 print(f"Total score: {sum}")
 
 # rolls: list[int] = list()
